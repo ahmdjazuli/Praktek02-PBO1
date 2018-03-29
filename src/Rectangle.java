e2fb9f2685df032c999de8b1f7ea25c9291645fa
Ahmad Jazuli <ahmadjazuli111@gmail.com>
2018-03-30 01:07:34 +0800

    Rectangle dan Aksi nya :v:

---
+++ src/Rectangle.java
@@ -0,0 +1,10 @@
+public class Rectangle {
+	double panjang,lebar;
+	
+	void cetakInfo() {
+		System.out.println("===============================");
+		System.out.println("Panjang = "+panjang);
+		System.out.println("Lebar   = "+lebar);
+		System.out.println("===============================");
+	}
+}
---
+++ src/RectangleAksi.java
@@ -0,0 +1,10 @@
+public class RectangleAksi {
+	public static void main(String[] args) {
+		// instance dolo
+		Rectangle persegi = new Rectangle();
+		persegi.panjang = 10;
+		persegi.lebar   = 5;
+		
+		persegi.cetakInfo();
+	}
+}
